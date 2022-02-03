7837e2c0b0d9939322043c6ed4b10282494f799f
Cheryl Ouyang <cherylo2389@gmail.com>
2022-02-03 17:55:15 +1100

    Output file info to csv & create directories

---
+++ optimiseDir.py
@@ -0,0 +1,53 @@
+import os
+import csv
+from datetime import datetime, timedelta
+
+def main():
+    print("Folder path/s:")
+    paths = []
+    while True:
+        path = input()
+        if path:
+            paths.append(path)
+        else:
+            break
+
+    with open('optimiseResults.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Folder path', 'Name', 'Date created', 'Size', 'Date last accessed', 'Accessed in the last month?'])
+
+        for path in paths:
+
+            newDocDirectory = "Docs"
+            # Docs -> docx, doc, xlxs
+            # Pdfs -> pdf
+            # Images -> png, jpg, jpeg
+            # Videos -> mp4
+            # Audio -> mp3, wav
+            newDirectories = ["Docs", "Pdfs", "Images", "Videos", "Audio"]
+            for dir in newDirectories:
+                newDirPath = os.path.join(path, dir)
+
+                # create folders for sorting file types
+                if not os.path.exists(newDirPath):
+                    os.makedirs(newDirPath)
+
+            with os.scandir(path) as dirEntries:
+                for entry in dirEntries:
+                    if entry.is_file():
+                        info = entry.stat()
+                        name = entry.name
+                        size = info.st_size
+                        dateCreated = datetime.fromtimestamp(info.st_ctime)
+                        # lastModified = datetime.fromtimestamp(info.st_mtime)
+                        lastAccessed = datetime.fromtimestamp(info.st_atime)
+
+                        if (datetime.now() - lastAccessed < timedelta(days = 30)):
+                            flag = 'Yes'
+                        else:
+                            flag = 'No'
+
+                        writer.writerow([path, name, dateCreated, size, lastAccessed, flag])
+
+if __name__ == "__main__":
+    main()
